43df78d3437965682f314c04ce25cbb7ecea2c38
Sanjeev Kushwaha <sanjeevk249116@gmail.com>
2024-04-12 19:59:41 +0530

    add

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import FormU from './components/FormU'
+
+function App() {
+  return (
+    <div className="App">
+     <FormU></FormU>
+    </div>
+  );
+}
+
+export default App;
